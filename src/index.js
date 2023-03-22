d5c8aaa25a858e0a22a329680cb5404376362ce2
David Zambrano <dzambrano.226@gmail.com>
2023-03-21 22:59:25 -0500

    :sparkles: Adding remove letter and letter colors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { fromEvent, Subject } from "rxjs";
 import WORDS_LIST from './wordsList.json';
 
 const letterRows = document.getElementsByClassName('letter-row');
+const messageText = document.getElementById('message-text');
+
 const onKeyDown$ = fromEvent(document, "keydown");
 let letterIndex = 0;
 let letterRowIndex = 0;
@@ -26,9 +28,58 @@ const keyDownObserver = {
     }
 }
 
+const deleteLetter = {
+    next: (event) => { 
+        const pressedKey = event.key;
+        if (pressedKey === 'Backspace' && letterIndex !== 0) {
+            let currentRow = letterRows[letterRowIndex];
+            let letterBox = currentRow.children[letterIndex - 1];
+            letterBox.textContent = '';
+            letterBox.classList.remove('filled-letter');
+            letterIndex--;
+            userAnswer.pop();
+            
+        }
+    }
+}
+
 const checkWord = {
     next: (event) => {
         if (event.key === 'Enter') {
+
+            const rightWordArray = Array.from(rightWord);
+            if (userAnswer.length !== 5) {
+                messageText.textContent = "Te faltan algunas letras!"
+                return;
+            } else {
+                messageText.textContent = "";
+            }
+
+            for (let index = 0; index < 5; index++) {
+                let letterColor = "";
+                let letterBox = Array.from(letterRows)[letterRowIndex].children[index];
+                let letterPosition = rightWordArray.indexOf(userAnswer[index]);
+
+                if (letterPosition === -1) {
+                    letterColor = "letter-gray";
+                } else {
+                    if (rightWordArray[index] === userAnswer[index]) {
+                        letterColor = "letter-green";
+                    } else {
+                        letterColor = "letter-yellow";
+                    }
+                }
+
+                letterBox.classList.add(letterColor);
+                
+            }
+
+            if (userAnswer.length === 5) {
+                letterIndex = 0;
+                letterRowIndex++;
+                userAnswer = [];
+            } 
+
             if (userAnswer.join("") === rightWord) {
                 userWinOrLosse$.next();
             }
@@ -38,6 +89,8 @@ const checkWord = {
 
 onKeyDown$.subscribe(keyDownObserver);
 onKeyDown$.subscribe(checkWord);
+onKeyDown$.subscribe(deleteLetter);
+
 userWinOrLosse$.subscribe(() => {
     let completeWordRow = Array.from(letterRows)[letterRowIndex];
     console.log(completeWordRow);
